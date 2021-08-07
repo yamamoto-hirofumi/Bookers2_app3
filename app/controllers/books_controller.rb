ba63a7c90f032f191744041da141283f80631f4f
yamamoto-hirofumi <sun.lionking.88@gmail.com>
2021-08-07 05:20:35 +0000

    [update]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -23,12 +23,17 @@ class BooksController < ApplicationController
 
   def edit
     @book = Book.find(params[:id])
+     if @book.user == current_user
+       render :edit
+     else
+      redirect_to books_path
+     end
   end
 
   def update
     @book = Book.find(params[:id])
     if @book.update(book_params)
-       redirect_to book_path(@book), natice: 'You have updated book successfully.'
+       redirect_to book_path(@book), notice: 'You have updated book successfully.'
     else
       render :edit
     end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :books
-  get 'homes/about' => 'homes#about'
+  get 'home/about' => 'homes#about'
   resources :users, only: [:show, :edit, :update, :index]
 end
