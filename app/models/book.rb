012fe824a31d5b1c2ac8509aa164fdfcaaa08dea
yamamoto-hirofumi <sun.lionking.88@gmail.com>
2021-08-09 12:35:06 +0000

    [7b]

--- app/models/book.rb
+++ app/models/book.rb
@@ -6,7 +6,7 @@ class Book < ApplicationRecord
    has_many :favorites, dependent: :destroy
    has_many :favorited_users, through: :favorites, source: :user
    has_many :book_comments, dependent: :destroy
-
+  
 
   def favorited_by?(user)
     favorites.where(user_id: user.id).exists?
